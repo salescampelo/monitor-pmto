14d5caeca6d78dab27745912273457aadabc5638
marcelscampelo <77081767+marcelscampelo@users.noreply.github.com>
2026-04-01 12:11:40 -0300

    Fix: estrutura correta para Vite/Vercel

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './Sentiment_Monitor_Coronel_Barbosa.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
